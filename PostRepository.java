37dc4b54ba0abc40a7aa174e8748ea1ef2b4aa5e
Pratiksha-kumari610 <pratikshak274@gmail.com>
2026-03-16 11:34:41 +0530

    Update PostRepository.java

--- PostRepository.java
+++ PostRepository.java
@@ -1,5 +1,5 @@
-package com.therajverma.blog.rest.api.repository;
-import com.therajverma.blog.rest.api.entity.Post;
+package com.thepratiksha.blog.rest.api.repository;
+import com.thepratiksha.blog.rest.api.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
